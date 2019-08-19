813ae03de158d25b8699b9f43baf5972eab398b3
Igor <dragonelf150991@gmail.com>
2019-08-19 23:17:39 +0300

    new file user added + half of the video

--- Project/src/index.js
+++ Project/src/index.js
@@ -3,14 +3,28 @@
 // чекбокс
 
 
-const checkbox = document.getElementById('discount-checkbox');
+const checkbox = document.querySelectorAll('.filter-check_checkbox');
+checkbox.forEach((elem) => {
 
+});
+for (let i = 0; i < checkbox.length; i++) {
+checkbox[i].addEventListener('change', function () {
+   if (this.checked === true){
+      this.nextElementSibling.classList.add('checked'); 
+    }  else{
+        this.nextElementSibling.classList.remove('checked');
+   }
+    });
+}
+
+
+/*const checkbox = document.getElementById('discount-checkbox');
 
 checkbox.addEventListener('change', function () {
-    console.log('test');
-    });
-
-
-   
-
-// end чекбокс
+    if (this.checked === true){
+      this.nextElementSibling.classList.add('checked'); 
+    }  else{
+        this.nextElementSibling.classList.remove('checked');
+    }
+    });*/
+ // end чекбокс
---
+++ Project/src/new.js
@@ -0,0 +1,15 @@
+'use strict'; 
+
+const user = ['CoolThin', 'AboutForb', 'AceTrimble', 'Aimilograel', 'Airgawaus', 'AloneNip', 'Alysiard', 'Annamaz', 
+            'Beneptica', 'BiggMessages', 'BookDream', 'Cepoinuo', 'Confenatr', 'Copensew', 'Davconfru', 'Demimexor', 
+            'DoomThe', 'DotTwitter', 'Editecomfl', 'Eldyenid', 'Fallsunti', 'FarerAngel', 'Featurenary', 'Fighterbeyo', 
+            'Findesi', 'Foretomedge', 'Formstronet', 'Freezingvi', 'Frogicalla', 'Getazzardol', 'Ghouletix', 'GnomeExclusive', 
+            'Goaffeas', 'Goldiame', 'Gossipping', 'GottaPenguin', 'GrantWord', 'GreenPierce', 'Griffonal', 'Headsmockd', 
+            'Imagesecu', 'Increrg', 'Infoxythi', 'Instantro', 'Islandedia', 'Jeansonso', 'Kopinchiq', 'LadyStyx', 'Latikilo', 
+            'Lawbootsyse', 'LordStand', 'Lovenelecon', 'Maidetylor', 'Measuperf', 'MindIan', 'Mingerop', 'Mobiliake', 
+            'MrBreaking', 'Muranebrid', 'Natellasse', 'Nothingerer', 'Noticentwo', 'Oneromedi', 'Ongwerkit', 'Orbonehi', 
+            'Poologican', 'PrankSon', 'Preappt', 'PressKing', 'RadiantRunning', 'Raetschanne', 'Rainthwa', 'Readire', 
+            'RecipeCrash', 'Rembler', 'Ridannelle', 'Rishipla', 'Scientor', 'Shadessent', 'Skaterfler', 'SportyWil', 
+            'Spyderbank', 'Spydereton', 'SpyderZerp', 'SpyMaid', 'Starcodyna', 'StormFantasy', 'Strater', 'Stylishipas', 
+            'TaBook', 'Tantisha', 'Techtye', 'Teenievera', 'TopicsFunny', 'Visentics', 'Vistaco', 'Wetheonelle', 
+            'Whiterdener', 'Willowthos', 'Wingsat'];
