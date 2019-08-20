ecb2637e25e2f28fd3ab866fb092b21bbc8f6902
DESKTOP-TG3CF6D\user <anastasia.00.qa@gmail.com>
2019-08-20 18:46:01 +0300

    Desktop workspace

--- Project/src/new.js
+++ Project/src/new.js
@@ -3,13 +3,26 @@
 const user = ['CoolThin', 'AboutForb', 'AceTrimble', 'Aimilograel', 'Airgawaus', 'AloneNip', 'Alysiard', 'Annamaz', 
             'Beneptica', 'BiggMessages', 'BookDream', 'Cepoinuo', 'Confenatr', 'Copensew', 'Davconfru', 'Demimexor', 
             'DoomThe', 'DotTwitter', 'Editecomfl', 'Eldyenid', 'Fallsunti', 'FarerAngel', 'Featurenary', 'Fighterbeyo', 
-            'Findesi', 'Foretomedge', 'Formstronet', 'Freezingvi', 'Frogicalla', 'Getazzardol', 'Ghouletix', 'GnomeExclusive', 
-            'Goaffeas', 'Goldiame', 'Gossipping', 'GottaPenguin', 'GrantWord', 'GreenPierce', 'Griffonal', 'Headsmockd', 
-            'Imagesecu', 'Increrg', 'Infoxythi', 'Instantro', 'Islandedia', 'Jeansonso', 'Kopinchiq', 'LadyStyx', 'Latikilo', 
-            'Lawbootsyse', 'LordStand', 'Lovenelecon', 'Maidetylor', 'Measuperf', 'MindIan', 'Mingerop', 'Mobiliake', 
-            'MrBreaking', 'Muranebrid', 'Natellasse', 'Nothingerer', 'Noticentwo', 'Oneromedi', 'Ongwerkit', 'Orbonehi', 
-            'Poologican', 'PrankSon', 'Preappt', 'PressKing', 'RadiantRunning', 'Raetschanne', 'Rainthwa', 'Readire', 
-            'RecipeCrash', 'Rembler', 'Ridannelle', 'Rishipla', 'Scientor', 'Shadessent', 'Skaterfler', 'SportyWil', 
-            'Spyderbank', 'Spydereton', 'SpyderZerp', 'SpyMaid', 'Starcodyna', 'StormFantasy', 'Strater', 'Stylishipas', 
-            'TaBook', 'Tantisha', 'Techtye', 'Teenievera', 'TopicsFunny', 'Visentics', 'Vistaco', 'Wetheonelle', 
-            'Whiterdener', 'Willowthos', 'Wingsat'];
+            'Findesi', 'Foretomedge', 'Formstronet', 'Freezingvi', 'Frogicalla', 'Getazzardol', 'Ghouletix', 
+            'GnomeExclusive', 'Goaffeas', 'Goldiame', 'Gossipping', 'GottaPenguin', 'GrantWord', 'GreenPierce', 
+            'Griffonal', 'Headsmockd', 'Imagesecu', 'Increrg', 'Infoxythi', 'Instantro', 'Islandedia', 'Jeansonso', 
+            'Kopinchiq', 'LadyStyx', 'Latikilo', 'Lawbootsyse', 'LordStand', 'Lovenelecon', 'Maidetylor', 'Measuperf', 
+            'MindIan', 'Mingerop', 'Mobiliake', 'MrBreaking', 'Muranebrid', 'Natellasse', 'Nothingerer', 'Noticentwo', 
+            'Oneromedi', 'Ongwerkit', 'Orbonehi', 'Poologican', 'PrankSon', 'Preappt', 'PressKing', 'RadiantRunning',
+            'Raetschanne', 'Rainthwa', 'Readire', 'RecipeCrash', 'Rembler', 'Ridannelle', 'Rishipla', 'Scientor', 
+            'Shadessent', 'Skaterfler', 'SportyWil', 'Spyderbank', 'Spydereton', 'SpyderZerp', 'SpyMaid', 'Starcodyna', 
+            'StormFantasy', 'Strater', 'Stylishipas', 'TaBook', 'Tantisha', 'Techtye', 'Teenievera', 'TopicsFunny', 
+            'Visentics', 'Vistaco', 'Wetheonelle', 'Whiterdener', 'Willowthos', 'Wingsat'];
+/*user.forEach( (elem) => {
+                console.log(elem.length);
+                        });*/
+
+user.forEach( (elem) => {
+        let minAmount = 9;   
+        if (minAmount < elem.length === true){
+           console.log(elem + ' ' + elem.length); 
+         }  else{
+             console.log('Short name');
+        }
+         }
+);
