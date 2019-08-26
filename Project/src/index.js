c1067bd438c6bb04c2a2474b6227cd35bdcc3840
Igor <dragonelf150991@gmail.com>
2019-08-26 23:37:28 +0300

    Cart added, start of the card add

--- Project/src/index.js
+++ Project/src/index.js
@@ -1,23 +1,16 @@
 'use strict'; 
 
 // чекбокс
-
-
 const checkbox = document.querySelectorAll('.filter-check_checkbox');
 checkbox.forEach((elem) => {
-
-});
-for (let i = 0; i < checkbox.length; i++) {
-checkbox[i].addEventListener('change', function () {
+elem.addEventListener('change', function () {
    if (this.checked === true){
       this.nextElementSibling.classList.add('checked'); 
     }  else{
         this.nextElementSibling.classList.remove('checked');
    }
     });
-}
-
-
+});
 /*const checkbox = document.getElementById('discount-checkbox');
 
 checkbox.addEventListener('change', function () {
@@ -28,3 +21,29 @@ checkbox.addEventListener('change', function () {
     }
     });*/
  // end чекбокс
+ // корзина
+const btnCart = document.getElementById('cart');
+const modalCart = document.querySelector('.cart');
+const closeBtn = document.querySelector('.cart-close');
+btnCart.addEventListener('click', () => {
+    modalCart.style.display = 'flex';
+    document.body.style.overflow = 'hidden';
+    });
+closeBtn.addEventListener('click', () => {
+    modalCart.style.display = 'none';
+    document.body.style.overflow = '';
+    });
+ // end корзина
+ // работа с товаром
+const cards = document.querySelectorAll('.goods .card');
+const cartWrapper = document.querySelector('.cart-wrapper');
+cards.forEach((card) =>{
+  const btn = card.querySelector('button');
+  btn.addEventListener('click', () => {
+    const cardClone = card.cloneNode(true);
+    cartWrapper.appendChild(cardClone);
+  })
+})
+
+
+ //end работа  с товаром
